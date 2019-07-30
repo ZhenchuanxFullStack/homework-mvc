ea8bc2c34f063b5a44783805ff2b4f8484f917e6
Ellen.Liu <116402157@qq.com>
2019-07-30 15:37:32 +0800

    feat: 添加配置文件

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const Koa = require('koa');
-const app = new Koa();
-const session = require('koa-session');
-const koaStatic = require('koa-static-prefix');
-const Router = require('koa-router');
-const bodyparser = require('koa-bodyparser');
-const path = require('path');
+const Koa = require('koa')
+const app = new Koa()
+const session = require('koa-session')
+const koaStatic = require('koa-static-prefix')
+const Router = require('koa-router')
+const bodyparser = require('koa-bodyparser')
+const path = require('path')
+
+const config = require('./config/default.js')
 
 app.use(koaStatic(path.join(__dirname, 'static'), {pathPrefix: '/static'}))
 
@@ -12,14 +14,12 @@ app.use(bodyparser())
 
 app.use(session({
   key: 'koa:sess',
-  signed: false
-}, app));
+  signed: false,
+}, app))
 
-var router = new Router();
-require('./routers/todo')(router);
+var router = new Router()
+require('./routers/todo')(router)
 
-app
-  .use(router.routes())
-  .use(router.allowedMethods())
+app.use(router.routes()).use(router.allowedMethods())
 
-module.exports = app.listen(3000);
+module.exports = app.listen(config.server.PORT)
