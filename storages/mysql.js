f57d1159c70204493bba85d7150b36f01f6bbbd7
dirk <inspiration520@gmail.com>
2019-09-03 21:21:23 +0800

    增加sinon注解，方便记忆

--- storages/mysql.js
+++ storages/mysql.js
@@ -1,28 +1,40 @@
-const mysql = require('mysql');
-const config = require('../.config').db;
+const mysql = require("mysql");
+const config = require("../.config").db;
 
-var pool  = mysql.createPool({
-  connectionLimit : 10,
-  host            : config.host,
-  user            : config.user,
-  password        : config.password,
-  database        : config.database
+var pool = mysql.createPool({
+  connectionLimit: 10,
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database
 });
 
-pool.query('select * from user limit 1', function(err, results, fields){
+pool.query("select * from user limit 1", function(err, results, fields) {
   /* istanbul ignore else  */
-  if (results) console.log("mysql online")
-  else console.error('mysql', err)
-})
+  if (results) console.log("mysql online");
+  else console.error("mysql", err);
+
+  // const i = 1;
+  // if (1 === i) {
+  //   console.log();
+  // } else if (2 === i) {
+  //   console.log();
+  // } else {
+  //   console.log();
+  // }
+  function noRun() {
+    const ii = 0;
+  }
+});
 
 module.exports = {
   query: async (sql, values) => {
     return new Promise((resolve, reject) => {
-      pool.query(sql, values, function(err, results){
+      pool.query(sql, values, function(err, results) {
         /* istanbul ignore if  */
-        if (err) reject(err)
-        else resolve(results)
-      })
-    })
+        if (err) reject(err);
+        else resolve(results);
+      });
+    });
   }
 };
--- test/http.test.js
+++ test/http.test.js
@@ -5,8 +5,12 @@ const path = require("path");
 
 const mysql = require("mysql");
 const sinon = require("sinon");
+// 使用sinon.stub接管mysql.createPool函数
 const stub = sinon.stub(mysql, "createPool");
+// 另外创建一个匿名函数，取名为query
 const query = sinon.stub();
+// mysql.createPool因为被接管，后续无法使用到pool.query方法，因此把新创建的query函数传给stub
+// 关于yields的使用方法，详见下边的测试用例
 stub.returns({ query });
 query.yields(null, 233);
 
@@ -55,6 +59,10 @@ describe("HTTP", () => {
       });
 
       it("POST /api/todo should return success", async () => {
+        // query为自定义的方法，在上边代码中，和pool.query关联，
+        // 调用yields方法，实际上是调用mysql.js中pool.query的回调函数
+        // pool.query("select * from user limit 1", function(err, results, fields) {})
+        // null 对应上边的err, 1 对应results
         query.yields(null, 1);
         // 判断参数是否符合预期
         var res = await request
