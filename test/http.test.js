a704cb6efd43017110e1136c10e9e6eb725a6503
xingw <xingw@mingyuanyun.com>
2019-09-03 16:15:25 +0800

    使用sinon接管MySQL数据处理

--- test/http.test.js
+++ test/http.test.js
@@ -1,85 +1,84 @@
-const should = require('should')
-const util = require('util')
-const fs = require('fs')
-const path = require('path')
-const app = require('../index.js')
+const should = require("should");
+const util = require("util");
+const fs = require("fs");
+const path = require("path");
+
+const mysql = require("mysql");
+const sinon = require("sinon");
+const stub = sinon.stub(mysql, "createPool");
+const query = sinon.stub();
+stub.returns({ query });
+
+const app = require("../index.js");
 const request = require("supertest").agent(app);
 
-const readFile = util.promisify(fs.readFile)
+const readFile = util.promisify(fs.readFile);
 
-describe('HTTP', () => {
-  after(function (done) {
+describe("HTTP", () => {
+  after(function(done) {
     app.close();
+    stub.restore();
     done();
   });
-  it('should return home page', async () =>  {
-    await request
-      .get('/')
-      .expect(/<html.*/)
+  it("should return home page", async () => {
+    await request.get("/").expect(/<html.*/);
   });
-  it('should return js file', async () =>  {
-    var jsFile = await readFile(path.join(__dirname, '../static/app.js'))
-    await request
-      .get('/static/app.js')
-      .expect(jsFile.toString())
-  })
+  it("should return js file", async () => {
+    var jsFile = await readFile(path.join(__dirname, "../static/app.js"));
+    await request.get("/static/app.js").expect(jsFile.toString());
+  });
 
-  describe('API', async () => {
-    describe('when not logged in', async () => {
-      it('POST /api/todo should require to auth', async () => {
-        var res = await request
-          .post('/api/todo')
-          .send({
-            todo: "一条新的todo"
-          })
-        should(res.statusCode).equal(401)
-      })
-
-      it('GET /api/todo should require to auth', async () => {
-        var res = await request.get('/api/todo')
-        should(res.statusCode).equal(401)
-      })
-
-    });
-
-    describe('when logged in', async () => {
-      it('should login and redirect to home page', async () => {
-        var res = await request
-          .post('/login')
-          .send({
-              user: 'eric',
-              password: 'asdfghjkl;'
-          })
-        // fake code
-        should(res.statusCode).equal(302)
+  describe("API", async () => {
+    describe("when not logged in", async () => {
+      it("POST /api/todo should require to auth", async () => {
+        var res = await request.post("/api/todo").send({
+          todo: "一条新的todo"
+        });
+        should(res.statusCode).equal(401);
       });
 
-      it('POST /api/todo should return success', async () => {
+      it("GET /api/todo should require to auth", async () => {
+        var res = await request.get("/api/todo");
+        should(res.statusCode).equal(401);
+      });
+    });
+
+    describe("when logged in", async () => {
+      it("should login and redirect to home page", async () => {
+        var res = await request.post("/login").send({
+          user: "eric",
+          password: "asdfghjkl;"
+        });
+        // fake code
+        should(res.statusCode).equal(302);
+      });
+
+      it("POST /api/todo should return success", async () => {
+        query.yields(null, 1);
+        // 判断参数是否符合预期
         var res = await request
-          .post('/api/todo')
+          .post("/api/todo")
           .send({
             todo: "一条新的todo"
           })
-          .expect(/\d+/)
-      })
+          .expect(/\d+/);
+      });
 
-      it('GET /api/todo should return all the data', async () => {
-        var res = await request
-          .get('/api/todo')
+      it("GET /api/todo should return all the data", async () => {
+        var res = await request.get("/api/todo");
         should(res.body).be.Array();
-      })
+      });
 
-      it('GET /api/todo/id should return only one todo', async () => {
-        var res = await request
-          .get('/api/todo/1')
-          .expect('一条新的todo')
-      })
+      it("GET /api/todo/id should return only one todo", async () => {
+        query.yields(null, [{ name: "一条新的todo" }]);
+        var res = await request.get("/api/todo/1").expect("一条新的todo");
+      });
 
-      it('GET /api/todo/id should return 404 if id not exists', async () => {
-        var res = await request
-          .get('/api/todo/x')
-        should(res.statusCode).equal(404)
-      })
-    })
-  })
-})
+      it("GET /api/todo/id should return 404 if id not exists", async () => {
+        query.yields(null, []);
+        var res = await request.get("/api/todo/x");
+        should(res.statusCode).equal(404);
+      });
+    });
+  });
+});
